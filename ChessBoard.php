b38e311d4a56948f1e2488ff7b6a04fb9dc0aecb
Nick Nygren <nnygren@shaw.ca>
2016-01-16 22:49:36 -0700

    consolidated php files

--- ChessBoard.php
+++ ChessBoard.php
@@ -16,6 +16,497 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>. */
 
+
+class Piece
+{
+	protected $direction;
+	protected $homerank;
+	protected $typecode;
+	protected $board;
+	public $x;
+	public $y;
+	public $isWhite;
+
+	public function addBoard($posx, $posy, $move)
+	{
+		$output = array();
+		$temp = new ChessBoard();
+		//echo $posx . "," . $posy . " to " . json_encode($move) . "\n";
+		if (($posy + $move[1] <= 8) && ($posy + $move[1] >= 1)
+			&& ($posx + $move[0] <= 8) && ($posx + $move[0] >= 1))
+		{
+			if (($this->board->get($posx + $move[0], $posy + $move[1]) == -1)
+				|| (($this->direction == 1) && ($this->board->get($posx + $move[0], $posy + $move[1]) > 6))
+				|| (($this->direction == -1) && ($this->board->get($posx + $move[0], $posy + $move[1]) < 6)))
+			{
+				$temp->copy($this->board);
+				$temp->set($posx, $posy, -1);
+				$temp->set($posx + $move[0], $posy + $move[1], $this->typecode);
+				$temp->setLastMove($posx, $posy, $posx + $move[0], $posy + $move[1]);
+				$temp->changeTurn();
+				$temp->checkKingRookHome();
+				$output[] = $temp->toString();
+			}
+		}
+		return $output;
+	}
+
+	public function searchDir($posx, $posy, $move)
+	{
+		$tx = 0;
+		$ty = 0;
+		$temp = array();
+		do
+		{
+			$tx = $tx + $move[0];
+			$ty = $ty + $move[1];
+			$diff = array($tx,$ty);
+			$temp = array_merge($temp, $this->addBoard($posx, $posy, $diff));
+		} while (($this->board->get($posx + $tx, $posy + $ty) == -1)
+			&& ($posx + $tx > 0)
+			&& ($posx + $tx <= 8)
+			&& ($posy + $ty > 0)
+			&& ($posy + $ty <= 8));
+		//echo "temp: " . json_encode($temp). "\n";
+		return $temp;
+	}
+
+	public function setColor($white) {
+		if ($white) {
+			$this->isWhite = true;
+			$this->direction = 1;
+			$this->homerank = 2;
+		}
+		else {
+			$this->isWhite = false;
+			$this->direction = -1;
+			$this->homerank = 7;
+		}
+	}
+
+	function __construct($xin, $yin, $board) {
+		$this->x = $xin;
+		$this->y = $yin;
+		$this->board = $board;
+	}
+}
+
+class Rook extends Piece
+{
+	public function generatePreList()
+	{
+		$output = array();
+		$moves = array();
+
+		$moves[] = (array(1,0));
+		$moves[] = (array(-1,0));
+		$moves[] = (array(0,1));
+		$moves[] = (array(0,-1));
+
+		foreach ($moves as $move)
+		{
+			$output = array_merge($output, $this->searchDir($this->x, $this->y, $move));
+		}
+
+		return $output;
+	}
+
+	public function typeString() { return "Rook,"; }
+
+	function __construct($xin, $yin, $board, $white)
+	{
+		parent::__construct($xin, $yin, $board);
+		$this->setColor($white);
+		if ($white) {
+			$this->typecode = ChessBoard::$WHITEROOK;
+		}
+		else {
+			$this->typecode = ChessBoard::$BLACKROOK;
+		}
+	}
+}
+
+class Pawn extends Piece
+{
+	protected $team;
+
+	public function generatePreList()
+	{
+		$output = array();
+		$temp = new ChessBoard();
+		$lastmove;
+		$lastmoved;
+
+		if (($this->y + $this->direction <= 8) && ($this->y + $this->direction >= 1) && ($this->board->get($this->x, $this->y + $this->direction) == -1))
+		{
+			$temp->copy($this->board);
+			$temp->set($this->x,$this->y,-1);
+			$temp->set($this->x,$this->y + $this->direction,$this->typecode);
+			$temp->setLastMove($this->x,$this->y,$this->x,$this->y + $this->direction);
+			$temp->changeTurn();
+			$temp->checkPromotion();
+			$output[] = $temp->toString();
+		}
+		if (($this->y == $this->homerank) && ($this->board->get($this->x, $this->y + $this->direction) == -1) && ($this->board->get($this->x, $this->y + (2 * $this->direction)) == -1))
+		{
+			$temp->copy($this->board);
+			$temp->set($this->x,$this->y,-1);
+			$temp->set($this->x,$this->y + 2*$this->direction,$this->typecode);
+			$temp->setLastMove($this->x,$this->y,$this->x,$this->y + 2*$this->direction);
+			$temp->changeTurn();
+			$temp->checkPromotion();
+			$output[] = ($temp->toString());
+		}
+		if (($this->y + $this->direction <= 8) && ($this->y + $this->direction >= 1) && ($this->x + 1 < 9))
+		{
+			if (($this->direction == 1) && ($this->board->get($this->x + 1, $this->y + $this->direction) > 6))
+			{
+				$temp->copy($this->board);
+				$temp->set($this->x,$this->y,-1);
+				$temp->set($this->x + 1, $this->y + $this->direction, $this->typecode);
+				$temp->setLastMove($this->x,$this->y,$this->x + 1, $this->y + $this->direction);
+				$temp->changeTurn();
+				$temp->checkPromotion();
+				$output[] = ($temp->toString());
+			}
+			else if (($this->direction == -1) && ($this->board->get($this->x + 1, $this->y + $this->direction) < 6) && ($this->board->get($this->x + 1, $this->y + $this->direction) >= 0))
+			{
+				$temp->copy($this->board);
+				$temp->set($this->x,$this->y,-1);
+				$temp->set($this->x + 1, $this->y + $this->direction, $this->typecode);
+				$temp->setLastMove($this->x,$this->y,$this->x + 1, $this->y + $this->direction);
+				$temp->changeTurn();
+				$temp->checkPromotion();
+				$output[] = ($temp->toString());
+			}
+		}
+		if (($this->y + $this->direction <= 8) && ($this->y + $this->direction >= 1) && ($this->x - 1 > 0))
+		{
+			if (($this->direction == 1) && ($this->board->get($this->x - 1, $this->y + $this->direction) > 6))
+			{
+				$temp->copy($this->board);
+				$temp->set($this->x,$this->y,-1);
+				$temp->set($this->x - 1, $this->y + $this->direction, $this->typecode);
+				$temp->setLastMove($this->x,$this->y,$this->x - 1, $this->y + $this->direction);
+				$temp->changeTurn();
+				$temp->checkPromotion();
+				$output[] = ($temp->toString());
+			}
+			else if (($this->direction == -1) && ($this->board->get($this->x - 1, $this->y + $this->direction) < 6) && ($this->board->get($this->x - 1, $this->y + $this->direction) >= 0))
+			{
+				$temp->copy($this->board);
+				$temp->set($this->x,$this->y,-1);
+				$temp->set($this->x - 1, $this->y + $this->direction, $this->typecode);
+				$temp->setLastMove($this->x,$this->y,x - 1, $this->y + $this->direction);
+				$temp->changeTurn();
+				$temp->checkPromotion();
+				$output[] = ($temp->toString());
+			}
+		}
+
+		$lastmove = $this->board->getLastMove();
+		$lastmoved = $this->board->get($lastmove[1] >> 4, $lastmove[1] & 0x0F);
+//		cout << "lastmove:" << $lastmove[0] << "," << $lastmove[1] << "\n";
+		if (ChessBoard::iptob($this->x+1,$this->y) == $lastmove[1] || ChessBoard::iptob($this->x-1,$this->y) == $lastmove[1])
+		{
+			if ($lastmove[0] - $lastmove[1] == 2*$this->direction)
+			{
+				if (($this->direction == -1 && $lastmoved == ChessBoard::$WHITEPAWN)
+					|| ($this->direction == 1 && $lastmoved == ChessBoard::$BLACKPAWN))
+				{
+					$temp->copy($this->board);
+					$temp->set($this->x,$this->y,-1);
+					$temp->set($lastmove[1] >> 4, $lastmove[1] & 0x0F,-1);
+					$temp->set($lastmove[1] >> 4, $this->y + $this->direction, $this->typecode);
+					$temp->setLastMove($this->x,$this->y,$lastmove[1] >> 4, $this->y + $this->direction);
+					$temp->changeTurn();
+					$output[] = ($temp->toString());
+				}
+			}
+		}
+		return $output;
+	}
+
+	public function typeString() { return "Pawn,"; }
+
+	function __construct($xin, $yin, $board, $white)
+	{
+		parent::__construct($xin, $yin, $board);
+		$this->setColor($white);
+		if ($white) {
+			$this->typecode = ChessBoard::$WHITEPAWN;
+		}
+		else {
+			$this->typecode = ChessBoard::$BLACKPAWN;
+		}
+	}
+}
+
+
+class Knight extends Piece
+{
+	public function generatePreList()
+	{
+		$moves = array();
+		$output = array();
+
+		$moves[] = (array(1,2));
+		$moves[] = (array(1,-2));
+		$moves[] = (array(2,1));
+		$moves[] = (array(2,-1));
+		$moves[] = (array(-1,2));
+		$moves[] = (array(-1,-2));
+		$moves[] = (array(-2,1));
+		$moves[] = (array(-2,-1));
+		foreach ($moves as $move)
+		{
+			$output = array_merge($output, $this->addBoard($this->x, $this->y, $move));
+		}
+
+		return $output;
+	}
+
+	public function typeString() { return "Knight,"; }
+
+	function __construct($xin, $yin, $board, $white)
+	{
+		parent::__construct($xin, $yin, $board);
+		$this->setColor($white);
+		if ($white) {
+			$this->typecode = ChessBoard::$WHITEKNIGHT;
+		}
+		else {
+			$this->typecode = ChessBoard::$BLACKKNIGHT;
+		}
+	}
+}
+
+
+class Queen extends Piece
+{
+	public function generatePreList()
+	{
+		$moves = array();
+		$output = array();
+
+		$moves[] = (array(1,0));
+		$moves[] = (array(-1,0));
+		$moves[] = (array(0,1));
+		$moves[] = (array(0,-1));
+		$moves[] = (array(1,1));
+		$moves[] = (array(1,-1));
+		$moves[] = (array(-1,1));
+		$moves[] = (array(-1,-1));
+
+		foreach ($moves as $move)
+		{
+			$output = array_merge($output, $this->searchDir($this->x, $this->y, $move));
+		}
+
+		return $output;
+	}
+
+	public function typeString() { return "Queen,"; }
+
+	function __construct($xin, $yin, $board, $white)
+	{
+		parent::__construct($xin, $yin, $board);
+		$this->setColor($white);
+		if ($white) {
+			$this->typecode = ChessBoard::$WHITEQUEEN;
+		}
+		else {
+			$this->typecode = ChessBoard::$BLACKQUEEN;
+		}
+	}
+}
+
+
+class Bishop extends Piece
+{
+	public function generatePreList()
+	{
+		$moves = array();
+		$output = array();
+
+		$moves[] = (array(1,1));
+		$moves[] = (array(1,-1));
+		$moves[] = (array(-1,1));
+		$moves[] = (array(-1,-1));
+
+		foreach ($moves as $move)
+		{
+			$output = array_merge($output, $this->searchDir($this->x, $this->y, $move));
+		}
+
+		return $output;
+	}
+
+	function typeString() { return "Bishop,"; }
+
+	function __construct($xin, $yin, $board, $white)
+	{
+		parent::__construct($xin, $yin, $board);
+		$this->setColor($white);
+		if ($white) {
+			$this->typecode = ChessBoard::$WHITEBISHOP;
+		}
+		else {
+			$this->typecode = ChessBoard::$BLACKBISHOP;
+		}
+	}
+
+}
+
+
+class King extends Piece
+{
+	public $recurse = false;
+
+	public function setRecurse($input) { $this->recurse = $input; }
+
+	public function generatePreList()
+	{
+		$temp = new ChessBoard();
+		$aboard = new ChessBoard();
+		
+		$moves = array();
+		$output = array();
+
+		$moves[] = (array(1,-1));
+		$moves[] = (array(1,0));
+		$moves[] = (array(1,1));
+		$moves[] = (array(-1,1));
+		$moves[] = (array(-1,-1));
+		$moves[] = (array(-1,0));
+		$moves[] = (array(0,1));
+		$moves[] = (array(0,-1));
+
+		foreach ($moves as $move)
+		{
+			$output = array_merge($output, $this->addBoard($this->x, $this->y, $move));
+		}
+
+		//the rest is castling conditions
+		if ($this->typecode == ChessBoard::$WHITEKING) 
+		{
+			//white king castling to the left
+			if ($this->x == 5 && $this->y == 1
+				&& $this->board->get(4,1) == -1 
+				&& $this->board->get(3,1) == -1 
+				&& $this->board->get(2,1) == -1 
+				&& $this->board->get(1,1) == ChessBoard::$WHITEROOK
+				&& !($this->board->castleData() & 64) 
+				&& !($this->board->castleData() & 32))
+			{
+				$aboard->copy($this->board);
+				$aboard->changeTurn();
+				if ($this->recurse && !ChessBoard::kingKillable("k,1,1," + $aboard->toString()) && !ChessBoard::leftCastleThroughCheck($this->board))
+				{
+					$temp->copy($this->board);
+					$temp->set(1, 1, -1);
+					$temp->set(3, 1, ChessBoard::$WHITEKING);
+					$temp->set(4, 1, ChessBoard::$WHITEROOK);
+					$temp->set(5, 1, -1);
+					$temp->setLastMove(5, 1, 3, 1);
+					$temp->changeTurn();
+					$temp->checkKingRookHome();
+					$output[] = ($temp->toString());
+				}
+			}
+			// white king castling to the right
+			if ($this->x == 5 && $this->y == 1
+				&& $this->board->get(6,1) == -1 
+				&& $this->board->get(7,1) == -1 
+				&& $this->board->get(8,1) == ChessBoard::$WHITEROOK
+				&& !($this->board->castleData() & 64) 
+				&& !($this->board->castleData() & 16))
+			{
+				$aboard->copy($this->board);
+				$aboard->changeTurn();
+				if ($this->recurse && !ChessBoard::kingKillable("k,1,1," + $aboard->toString()) && !ChessBoard::rightCastleThroughCheck($this->board))
+				{
+					$temp->copy($this->board);
+					$temp->set(8, 1, -1);
+					$temp->set(7, 1, ChessBoard::$WHITEKING);
+					$temp->set(6, 1, ChessBoard::$WHITEROOK);
+					$temp->set(5, 1, -1);
+					$temp->setLastMove(5, 1, 7, 1);
+					$temp->changeTurn();
+					$temp->checkKingRookHome();
+					$output[] = ($temp->toString());
+				}
+			}
+		}	
+		if ($this->typecode == ChessBoard::$BLACKKING) 
+		{
+			// black king castling to the left
+			if ($this->x == 5 && $this->y == 8
+				&& $this->board->get(4,8) == -1 
+				&& $this->board->get(3,8) == -1 
+				&& $this->board->get(2,8) == -1 
+				&& $this->board->get(1,8) == ChessBoard::$BLACKROOK
+				&& !($this->board->castleData() & 4) 
+				&& !($this->board->castleData() & 2))
+			{
+				$aboard->copy($this->board);
+				$aboard->changeTurn();
+				if ($this->recurse && !ChessBoard::kingKillable("k,1,1," + $aboard->toString()) && !ChessBoard::leftCastleThroughCheck($this->board))
+				{
+					$temp->copy($this->board);
+					$temp->set(1, 8, -1);
+					$temp->set(3, 8, ChessBoard::$BLACKKING);
+					$temp->set(4, 8, ChessBoard::$BLACKROOK);
+					$temp->set(5, 8, -1);
+					$temp->setLastMove(5, 8, 3, 8);
+					$temp->changeTurn();
+					$temp->checkKingRookHome();
+					$output[] = ($temp->toString());
+				}
+			}
+			// black king castling to the right
+			if ($this->x == 5 && $this->y == 8
+				&& $this->board->get(6,8) == -1 
+				&& $this->board->get(7,8) == -1 
+				&& $this->board->get(8,8) == ChessBoard::$BLACKROOK
+				&& !($this->board->castleData() & 4) 
+				&& !($this->board->castleData() & 1))
+			{
+				$aboard->copy($this->board);
+				$aboard->changeTurn();
+				if ($this->recurse && !ChessBoard::kingKillable("k,1,1," + $aboard->toString()) && !ChessBoard::rightCastleThroughCheck($this->board))
+				{
+					$temp->copy($this->board);
+					$temp->set(8, 8, -1);
+					$temp->set(7, 8, ChessBoard::$BLACKKING);
+					$temp->set(6, 8, ChessBoard::$BLACKROOK);
+					$temp->set(5, 8, -1);
+					$temp->setLastMove(5, 8, 7, 8);
+					$temp->changeTurn();
+					$temp->checkKingRookHome();
+					$output[] = ($temp->toString());
+				}
+			}
+		}	
+
+		return $output;
+	}
+
+	public function typeString() { return "King,"; }
+
+	function __construct($xin, $yin, $board, $white)
+	{
+		parent::__construct($xin, $yin, $board);
+		$this->setColor($white);
+		if ($white) {
+			$this->typecode = ChessBoard::$WHITEKING;
+		}
+		else {
+			$this->typecode = ChessBoard::$BLACKKING;
+		}
+	}
+}
+
 class ChessBoard {
 	protected $whiteturn;
 	protected $castle;
@@ -372,6 +863,175 @@ class ChessBoard {
 			$this->data[$i][$j] = -1;
 		}
 	}
+
+	public static function toAlg($input)
+	{
+		if ($input[0]== '1') { $temp = "A"; }
+		else if ($input[0] == '2') { $temp = "B"; }
+		else if ($input[0] == '3') { $temp = "C"; }
+		else if ($input[0] == '4') { $temp = "D"; }
+		else if ($input[0] == '5') { $temp = "E"; }
+		else if ($input[0] == '6') { $temp = "F"; }
+		else if ($input[0] == '7') { $temp = "G"; }
+		else if ($input[0] == '8') { $temp = "H"; }
+		else { $temp = "0"; }
+		$temp = $temp . substr($input,1,1);
+		return $temp;
+	}
+
+	public static function genList($original, $recurse) {
+		$board = new ChessBoard();
+		$board->loadString($original);
+		$outlist = array();
+		//echo var_dump(debug_backtrace()) . "\n";
+		for ($i = 1; $i < 9; $i++)
+			for ($j = 1; $j < 9; $j++)
+			{
+				if ($board->whiteTurn())
+				{
+					if ($board->get($i,$j) == ChessBoard::$WHITEPAWN)
+					{
+						$piece = new Pawn($i, $j, $board, true);
+					}
+					else if ($board->get($i,$j) == ChessBoard::$WHITEKING)
+					{
+						$piece = new King($i, $j, $board, true);
+						$piece->setRecurse(recurse);
+					}
+					else if ($board->get($i,$j) == ChessBoard::$WHITEKNIGHT)
+					{
+						$piece = new Knight($i, $j, $board, true);
+					}
+					else if ($board->get($i,$j) == ChessBoard::$WHITEROOK)
+					{
+						$piece = new Rook($i, $j, $board, true);
+					}
+					else if ($board->get($i,$j) == ChessBoard::$WHITEBISHOP)
+					{
+						$piece = new Bishop($i, $j, $board, true);
+					}
+					else if ($board->get($i,$j) == ChessBoard::$WHITEQUEEN)
+					{
+						$piece = new Queen($i, $j, $board, true);
+					}
+					else { $piece = 0; }
+				}
+				else
+				{
+					if ($board->get($i,$j) == ChessBoard::$BLACKPAWN)
+					{
+						$piece = new Pawn($i, $j, $board, false);
+					}
+					else if ($board->get($i,$j) == ChessBoard::$BLACKKING)
+					{
+						$piece = new King($i, $j, $board, false);
+						$piece->setRecurse(recurse);
+					}
+					else if ($board->get($i,$j) == ChessBoard::$BLACKKNIGHT)
+					{
+						$piece = new Knight($i, $j, $board, false);
+					}
+					else if ($board->get($i,$j) == ChessBoard::$BLACKROOK)
+					{
+						$piece = new Rook($i, $j, $board, false);
+					}
+					else if ($board->get($i,$j) == ChessBoard::$BLACKBISHOP)
+					{
+						$piece = new Bishop($i, $j, $board, false);
+					}
+					else if ($board->get($i,$j) == ChessBoard::$BLACKQUEEN)
+					{
+						$piece = new Queen($i, $j, $board, false);
+					}
+					else { $piece = 0; }
+				}
+
+				if ($piece != 0)
+				{
+					$list = $piece->generatePreList();
+					foreach ($list as $row)
+					{
+						$temp = $piece->typeString();
+						$temp .= self::toAlg(substr($row,68,2)) . ",";
+						$temp .= self::toAlg(substr($row,70,2)) . ",";
+						$temp .= $row;
+						$outlist[] = $temp;
+					}
+					$piece = 0;
+				}
+			}
+
+			return $outlist;
+		}
+
+	public static function kingKillable($input) {
+		$rec = explode(',', $input);
+		$list = self::genList($rec[3], false);
+
+		foreach ($list as $row) {
+			$rec = explode(',', $row);
+			$board = $rec[3];
+			if (substr($board,10,2) == "00" || substr($board,58,2) == "00")
+			{
+				return true; 
+			}
+		}
+		return false;
+	}
+
+	public static function leftCastleThroughCheck($input)
+	{
+		$alter = new ChessBoard();
+		$alter->copy($input);
+		$alter->changeTurn();
+		if ($input->whiteTurn())
+		{
+			$alter->set(5, 1, -1);
+			$alter->set(4, 1, ChessBoard::$WHITEKING);
+
+		}
+		else
+		{
+			$alter->set(5, 8, -1);
+			$alter->set(4, 8, ChessBoard::$BLACKKING);
+		}
+		return self::kingKillable("k,1,1," + $alter->toString());
+
+	}
+
+	public static function rightCastleThroughCheck($input)
+	{
+		$alter = new ChessBoard();
+		$alter->copy($input);
+		$alter->changeTurn();
+		if ($input->whiteTurn())
+		{
+			$alter->set(5, 1, -1);
+			$alter->set(6, 1, ChessBoard::$WHITEKING);
+		}
+		else
+		{
+			$alter->set(5, 8, -1);
+			$alter->set(6, 8, ChessBoard::$BLACKKING);
+		}
+		return self::kingKillable("K,1,1," + $alter->toString());
+
+
+	}
+
+	public static function moves($input) {
+		if (strlen($input) > 0) { $list = self::genList($input, false); }
+		else { $list = self::genList("AA1121314151617181122232425262728217273747576777871828384858687888000000",false); }
+
+		foreach ($list as $row)
+		{
+			if (self::kingKillable($row) == false) { echo  $row . "\n";  }
+		}
+
+		return $list;
+	}
 }
+
+ChessBoard::moves("BB8100531151850000234345546273830025354655677787008844574758000000224111");
 
 ?>
